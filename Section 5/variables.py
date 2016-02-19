ad50443ff5af38ca2703bb3675d885179f42e7f9
Jim Veneskey <github@strifecrafter.com>
2016-02-19 12:29:20 -0500

    floating point and integers in P3

---
+++ Section 5/variables.py
@@ -0,0 +1,9 @@
+# variables.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    num = round(42 / 9, 2)
+    print("num is {}, its type is {}".format(num, type(num)))
+
+if __name__ == "__main__": main()
