c2ae634852c6136928fe25d80e7f864ac62446d4
Jim Veneskey <github@strifecrafter.com>
2016-02-22 06:49:06 -0500

    if and elif

---
+++ Section 6/elif_example.py
@@ -0,0 +1,16 @@
+# conditionals.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    v = 'three'
+    if v == 'one':
+        print('v is one')
+    elif v == 'two':
+        print('v is two')
+    elif v == 'three':
+        print('v is three')
+    else:
+        print('v is something else')
+        
+if __name__ == "__main__": main()
