e199d8c20581d20b81a25b1292535bb303c29417
Jim Veneskey <github@strifecrafter.com>
2016-02-22 06:31:34 -0500

    simple conditionals

---
+++ Section 6/conditionals.py
@@ -0,0 +1,13 @@
+# conditionals.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    a, b = 2, 1
+    # 'a < b' is the conditional
+    if a < b:
+        print('this is true')
+    else:
+        print('this is not true')
+
+if __name__ == "__main__": main()
