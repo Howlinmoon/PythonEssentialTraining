d5aee8ad043452d0370a26f91b82c7225ca27bfd
Jim Veneskey <github@strifecrafter.com>
2016-02-22 12:40:24 -0500

    FOR LOOPS with enumeration - good stuff

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PythonEssentialTraining
 
-Left off with 07_03
+Left off with 07_04
---
+++ Section 7/for_loop_enum.py
@@ -0,0 +1,16 @@
+# for.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    fh = open('lines.txt')
+    for index, line in enumerate(fh.readlines()):
+        print(index, line, end = '')
+        
+    s = 'this is a sad little string'
+    for i, c in enumerate(s):
+        print(i, c)
+        if c == 's':
+            print('index {} is an s'.format(i))
+
+if __name__ == "__main__": main()
