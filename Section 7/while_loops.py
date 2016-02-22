e5ef26777e69da5c283fbadcb4add978a08c3630
Jim Veneskey <github@strifecrafter.com>
2016-02-22 12:25:34 -0500

    house cleaning

--- Section 7/while_loops.py
+++ Section 7/while_loops.py
@@ -7,7 +7,7 @@ def main():
     # the sum of two elements defines the next set
     a, b = 0, 1
     while b < 50:
-        print(b, end=' ')
+        print(b, end =' ')
         a, b = b, a + b
 
 if __name__ == "__main__": main()
