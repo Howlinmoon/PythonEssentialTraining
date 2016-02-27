e3483adeffc55bf153f0f78e1570f3af50ce6955
Howlinmoon <github@strifecrafter.com>
2016-02-27 08:02:19 -0500

    Experimenting with files

---
+++ Section 15/reading_writing.py
@@ -0,0 +1,17 @@
+# files.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    buffersize = 50000
+    infile = open('bigfile.txt', 'r')
+    outfile = open('new.txt', 'w')
+    buffer = infile.read(buffersize)
+    while len(buffer):
+        outfile.write(buffer)
+        print('.', end = '')
+        buffer = infile.read(buffersize)
+
+    print()
+    print("Done!")
+if __name__ == "__main__": main()
